9df2f6e601b40f443154dd416122996f2396699d
Alexander Bai <vhurryharry@gmail.com>
2019-10-04 23:18:29 -0400

    Fixed deploy keygen.

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,4 +4,3 @@ set :application, "school"
 set :repo_url, "git@bitbucket.org:janestreetmedia/school.git"
 set :linked_files, []
 set :file_permissions_paths, -> { [fetch(:var_path), fetch(:cache_path)] }
-set :ssh_options, { :forward_agent => true }
