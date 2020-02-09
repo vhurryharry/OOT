0482ed81ce58f4a483366977fd0e343956745688
Alexander Bai <vhurryharry@gmail.com>
2020-02-10 00:50:58 +1000

    Upgraded the typescript version.

--- customer/src/app/layout/blank/blank.component.ts
+++ customer/src/app/layout/blank/blank.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
 
 @Component({
-  selector: "app-blank",
-  templateUrl: "./blank.component.html"
+    selector: "app-blank",
+    templateUrl: "./blank.component.html"
 })
 export class BlankComponent implements OnInit {
-  @ViewChild("root") root;
+    @ViewChild("root", { static: false }) root;
 
-  constructor() {}
+    constructor() { }
 
-  ngOnInit() {}
+    ngOnInit() { }
 }
