d43e118be89f4d55c8b2722e8c61f672e011abfd
Alexander Bai <vhurryharry@gmail.com>
2020-02-10 03:16:40 +1000

    Fixed linting issues.

--- customer/src/app/layout/shared/shared.module.ts
+++ customer/src/app/layout/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 
 import { HeaderComponent } from "./header/header.component";
 import { CommonModule } from "@angular/common";
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from "./footer/footer.component";
 
 @NgModule({
     imports: [CommonModule],
