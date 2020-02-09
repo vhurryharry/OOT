c236a5dd167c7eb1bb7405c21db87026ea32ea71
Alexander Bai <vhurryharry@gmail.com>
2020-02-10 03:13:50 +1000

    Updated some styles.

--- customer/src/app/app.module.ts
+++ customer/src/app/app.module.ts
@@ -4,18 +4,16 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { CondensedComponent } from "./layout/condensed/condensed.component";
-import { FooterComponent } from "./layout/shared/footer/footer.component";
 import { BlankComponent } from "./layout/blank/blank.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CondensedComponent,
-    FooterComponent,
-    BlankComponent
-  ],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CondensedComponent,
+        BlankComponent
+    ],
+    imports: [BrowserModule, AppRoutingModule],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- customer/src/app/layout/shared/shared.module.ts
+++ customer/src/app/layout/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 
 import { HeaderComponent } from "./header/header.component";
 import { CommonModule } from "@angular/common";
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [HeaderComponent],
-  exports: [HeaderComponent]
+    imports: [CommonModule],
+    declarations: [HeaderComponent, FooterComponent],
+    exports: [HeaderComponent, FooterComponent]
 })
-export class SharedModule {}
+export class SharedModule { }
