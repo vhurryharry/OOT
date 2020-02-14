50e8fe43c861833e078463203a6d4b7973fd3754
Alexander Bai <vhurryharry@gmail.com>
2020-02-14 13:46:34 +1000

    Header/Footer modification.

--- customer/src/app/home/home.component.ts
+++ customer/src/app/home/home.component.ts
@@ -3,34 +3,34 @@ import { Component, OnInit } from "@angular/core";
 declare var $: any;
 
 @Component({
-  selector: "app-homepage",
-  templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.scss"]
+    selector: "app-homepage",
+    templateUrl: "./home.component.html",
+    styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+    constructor() { }
 
-  ngOnInit() {
-    // Image Gallery configuration
-    $(".multi-item-carousel .carousel-item").each(() => {
-      let next = $(this).next();
-      if (!next.length) {
-        next = $(this).siblings(":first");
-      }
-      next
-        .children(":first-child")
-        .clone()
-        .appendTo($(this));
-    });
-    $(".multi-item-carousel .carousel-item").each(() => {
-      let prev = $(this).prev();
-      if (!prev.length) {
-        prev = $(this).siblings(":last");
-      }
-      prev
-        .children(":nth-last-child(2)")
-        .clone()
-        .prependTo($(this));
-    });
-  }
+    ngOnInit() {
+        // Image Gallery configuration
+        $(".multi-item-carousel .carousel-item").each((index, item) => {
+            let next = $(item).next();
+            if (!next.length) {
+                next = $(item).siblings(":first");
+            }
+            next
+                .children(":first-child")
+                .clone()
+                .appendTo($(item));
+        });
+        $(".multi-item-carousel .carousel-item").each((index, item) => {
+            let prev = $(item).prev();
+            if (!prev.length) {
+                prev = $(item).siblings(":last");
+            }
+            prev
+                .children(":nth-last-child(2)")
+                .clone()
+                .prependTo($(item));
+        });
+    }
 }
