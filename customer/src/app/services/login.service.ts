3c61c17753ea18e157141ce5d5d05ce646b98347
Alexander Bai <vhurryharry@gmail.com>
2020-02-18 03:29:25 +1000

    Fixed linting issue.

--- customer/src/app/services/login.service.ts
+++ customer/src/app/services/login.service.ts
@@ -6,103 +6,103 @@ import { Observable, pipe, throwError } from "rxjs";
 import { map } from "rxjs/operators";
 
 export interface IUserInfo {
-    id: number;
-    metadata: string;
-    type: string;
-    email: string;
-    status: string;
-    firstName: string;
-    lastName: string;
-    acceptsMarketing: boolean;
-    tagline: string;
-    occupation: string;
-    birthDate: Date;
-    mfa: string;
+  id: number;
+  metadata: string;
+  type: string;
+  email: string;
+  status: string;
+  firstName: string;
+  lastName: string;
+  acceptsMarketing: boolean;
+  tagline: string;
+  occupation: string;
+  birthDate: Date;
+  mfa: string;
 }
 
 @Injectable()
 export class LoginService {
-    private authURL: string = environment.baseURL + `/api/auth`;
-    currentUser: IUserInfo;
-    authError: string;
-    redirectUrl: string;
+  private authURL: string = environment.baseURL + `/api/auth`;
+  currentUser: IUserInfo;
+  authError: string;
+  redirectUrl: string;
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-    isLoggedIn(): boolean {
-        if (localStorage.getItem("oot_user_token")) {
-            if (!this.currentUser) {
-                this.currentUser = JSON.parse(localStorage.getItem("oot_user_token"));
-            }
+  isLoggedIn(): boolean {
+    if (localStorage.getItem("oot_user_token")) {
+      if (!this.currentUser) {
+        this.currentUser = JSON.parse(localStorage.getItem("oot_user_token"));
+      }
 
-            return true;
-        }
-        return false;
+      return true;
     }
+    return false;
+  }
 
-    getCurrentUser(): IUserInfo {
-        return this.currentUser;
-    }
+  getCurrentUser(): IUserInfo {
+    return this.currentUser;
+  }
 
-    getCurrentUserId(): number {
-        return this.currentUser.id;
-    }
+  getCurrentUserId(): number {
+    return this.currentUser.id;
+  }
 
-    authenticate(email, password): Observable<IUserInfo> {
-        return this.http
-            .post<any>(`${this.authURL}/customer-login`, {
-                email,
-                password
-            })
-            .pipe<any>(
-                map(response => {
-                    if (response && response.success) {
-                        localStorage.setItem(
-                            "oot_user_token",
-                            JSON.stringify(response.user)
-                        );
-
-                        this.currentUser = response.user;
-                        this.authError = null;
-
-                        return this.currentUser;
-                    } else {
-                        this.authError = response.error;
-                        this.currentUser = null;
-
-                        throw new Error(response.error);
-                    }
-                })
+  authenticate(email, password): Observable<IUserInfo> {
+    return this.http
+      .post<any>(`${this.authURL}/customer-login`, {
+        email,
+        password
+      })
+      .pipe<any>(
+        map(response => {
+          if (response && response.success) {
+            localStorage.setItem(
+              "oot_user_token",
+              JSON.stringify(response.user)
             );
-    }
 
-    logOut(): void {
-        localStorage.removeItem("oot_user_token");
-        this.currentUser = null;
-    }
+            this.currentUser = response.user;
+            this.authError = null;
 
-    register(userInfo: any): Observable<IUserInfo> {
-        return this.http
-            .post<any>(`${this.authURL}/customer-register`, userInfo)
-            .pipe<any>(
-                map(response => {
-                    if (response && response.success === true) {
-                        localStorage.setItem(
-                            "oot_user_token",
-                            JSON.stringify(response.user)
-                        );
+            return this.currentUser;
+          } else {
+            this.authError = response.error;
+            this.currentUser = null;
 
-                        this.currentUser = response.user;
-                        this.authError = null;
+            throw new Error(response.error);
+          }
+        })
+      );
+  }
 
-                        return this.currentUser;
-                    } else {
-                        this.authError = response.error;
-                        this.currentUser = null;
+  logOut(): void {
+    localStorage.removeItem("oot_user_token");
+    this.currentUser = null;
+  }
 
-                        throw new Error(response.error);
-                    }
-                })
+  register(userInfo: any): Observable<IUserInfo> {
+    return this.http
+      .post<any>(`${this.authURL}/customer-register`, userInfo)
+      .pipe<any>(
+        map(response => {
+          if (response && response.success === true) {
+            localStorage.setItem(
+              "oot_user_token",
+              JSON.stringify(response.user)
             );
-    }
+
+            this.currentUser = response.user;
+            this.authError = null;
+
+            return this.currentUser;
+          } else {
+            this.authError = response.error;
+            this.currentUser = null;
+
+            throw new Error(response.error);
+          }
+        })
+      );
+  }
 }
