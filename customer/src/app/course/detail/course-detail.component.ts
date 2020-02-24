294fd6f8e3a3fb04830573925e8329e5a98af96b
Alexander Bai <vhurryharry@gmail.com>
2020-02-25 03:27:39 +1000

    Fixed linting issues.

--- customer/src/app/course/detail/course-detail.component.ts
+++ customer/src/app/course/detail/course-detail.component.ts
@@ -70,7 +70,7 @@ export class CourseDetailComponent implements OnInit {
         .clone()
         .prependTo($(item));
     });
-  };
+  }
 
   ngOnInit() {}
 }
--- customer/src/app/home/home.component.ts
+++ customer/src/app/home/home.component.ts
@@ -43,7 +43,7 @@ export class HomeComponent implements OnInit {
         .clone()
         .prependTo($(item));
     });
-  };
+  }
 
   ngOnInit() {
     this.initCarousels("home-gallery");
